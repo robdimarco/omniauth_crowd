a5084460c63f844597216cb137d58fb6113c75d1
Comb Hua <combhua@gmail.com>
2012-10-31 17:03:00 -0500

    added method to use Nokogiri to form request body to ensure valid xml

--- lib/omniauth/strategies/crowd/crowd_validator.rb
+++ lib/omniauth/strategies/crowd/crowd_validator.rb
@@ -68,11 +68,19 @@ module OmniAuth
           http.verify_mode = OpenSSL::SSL::VERIFY_NONE if http.use_ssl? && @configuration.disable_ssl_verification?
           http.start do |c|
             req = Net::HTTP::Post.new("#{@authentiction_uri.path}?#{@authentiction_uri.query}")
-            req.body = AUTHENTICATION_REQUEST_BODY % @password
+            req.body = make_authentication_request_body(@password)
             req.basic_auth @configuration.crowd_application_name, @configuration.crowd_password
             req.add_field 'Content-Type', 'text/xml'
             http.request(req)
           end
+        end
+
+        # create the body using Nokogiri so proper encoding of passwords can be ensured
+        def make_authentication_request_body(password)
+          request_body = Nokogiri::XML(AUTHENTICATION_REQUEST_BODY)
+          password_value = request_body.at_css "value"
+          password_value.content = password
+          return request_body.root.to_s # return the body without the xml header
         end
       end
     end
